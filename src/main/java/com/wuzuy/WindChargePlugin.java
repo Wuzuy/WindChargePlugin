2399ad187d8a77155dcb02930277d63669fa22d7
Wuzuy <ffxtreme9@gmail.com>
2024-07-29 11:29:52 -0300

    WindChargePlugin

--- src/main/java/com/wuzuy/WindChargePlugin.java
+++ src/main/java/com/wuzuy/WindChargePlugin.java
@@ -4,10 +4,9 @@ import com.wuzuy.listeners.WindChargeListener;
 import org.bukkit.Particle;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Projectile;
-import org.bukkit.entity.TNTPrimed;
+import org.bukkit.entity.Firework;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.Vector;
+import org.bukkit.scheduler.BukkitRunnable;
 
 public class WindChargePlugin extends JavaPlugin {
 
@@ -27,29 +26,25 @@ public class WindChargePlugin extends JavaPlugin {
         double explosionForce = config.getDouble("windcharge.explosionForce", 1.0);
         boolean addParticles = config.getBoolean("windcharge.addParticles", true);
         double projectileSpeed = config.getDouble("windcharge.projectileSpeed", 1.0);
-        String particleTypeName = config.getString("windcharge.particleType", "FLAME");
 
-        // Converte o nome da partícula para o tipo de partícula correspondente
-        Particle particleType;
-        try {
-            particleType = Particle.valueOf(particleTypeName.toUpperCase());
-        } catch (IllegalArgumentException e) {
-            getLogger().warning("Tipo de partícula inválido: " + particleTypeName + ". Usando FLAME como padrão.");
-            particleType = Particle.FLAME;
-        }
+        // Criar um projétil de fogos de artifício como exemplo
+        Firework firework = player.getWorld().spawn(player.getLocation().add(player.getLocation().getDirection().multiply(2)), Firework.class);
+        firework.setVelocity(player.getLocation().getDirection().multiply(projectileSpeed));
 
         if (addParticles) {
-            player.getWorld().spawnParticle(particleType, player.getLocation(), 100);
+            // Adicionar partículas ao projétil
+            player.getWorld().spawnParticle(Particle.EXPLOSION, firework.getLocation(), 10);
         }
 
-        // Cria uma explosão TNT para simular a força da explosão
-        TNTPrimed tnt = player.getWorld().spawn(player.getLocation(), TNTPrimed.class);
-        tnt.setYield((float) explosionForce);
-        tnt.setFuseTicks(0); // Detona imediatamente
-
-        // Cria um projétil de exemplo e ajusta a velocidade
-        Projectile projectile = player.getWorld().spawn(player.getLocation(), org.bukkit.entity.Arrow.class);
-        Vector direction = player.getLocation().getDirection().normalize().multiply(projectileSpeed);
-        projectile.setVelocity(direction);
+        // Agendar uma tarefa para criar a explosão após um curto período
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (firework.isValid()) {
+                    firework.getWorld().createExplosion(firework.getLocation(), (float) explosionForce, false, false);
+                    firework.remove(); // Remover o projétil após a explosão
+                }
+            }
+        }.runTaskLater(this, 20L); // Espera 1 segundo antes de criar a explosão
     }
 }
